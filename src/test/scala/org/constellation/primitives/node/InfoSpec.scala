cffdeaaf252d2e1771f7f4e60df7a738f230d88a
Maciej Muszyński <30803273+TheMMaciek@users.noreply.github.com>
2021-01-27 14:55:26 +0100

    Fix unit test for Info class

--- src/test/scala/org/constellation/primitives/node/InfoSpec.scala
+++ src/test/scala/org/constellation/primitives/node/InfoSpec.scala
@@ -12,7 +12,7 @@ class InfoSpec extends FunSpec with Matchers {
   describe("valid json object string") {
 
     val validJson =
-      """{"id":{"hex":"4256bee35df47fd49885d0a5931178e9d0cf33f314b74680646cd391a96ba25fc33cc7c3e1274ffc29a64828d5feeb2f99242e0dffb13c006250d35b3f810551"},"ip":{"host":"35.236.125.81","port":9001},"status":"PendingDownload","reputation":0}"""
+      """{"id":{"hex":"4256bee35df47fd49885d0a5931178e9d0cf33f314b74680646cd391a96ba25fc33cc7c3e1274ffc29a64828d5feeb2f99242e0dffb13c006250d35b3f810551"},"ip":{"host":"35.236.125.81","port":9001},"status":"PendingDownload","reputation":0,"alias":"someAlias"}"""
 
     it("parses as an object") {
       decode[Info](validJson) shouldBe a[Right[_, _]]
@@ -22,7 +22,7 @@ class InfoSpec extends FunSpec with Matchers {
   describe("valid json array string") {
 
     val validJson =
-      """[{"id":{"hex":"4256bee35df47fd49885d0a5931178e9d0cf33f314b74680646cd391a96ba25fc33cc7c3e1274ffc29a64828d5feeb2f99242e0dffb13c006250d35b3f810551"},"ip":{"host":"35.236.125.81","port":9001},"status":"PendingDownload","reputation":0}]"""
+      """[{"id":{"hex":"4256bee35df47fd49885d0a5931178e9d0cf33f314b74680646cd391a96ba25fc33cc7c3e1274ffc29a64828d5feeb2f99242e0dffb13c006250d35b3f810551"},"ip":{"host":"35.236.125.81","port":9001},"status":"PendingDownload","reputation":0,"alias":"someAlias"}]"""
 
     it("parses as list of objects") {
       decode[List[Info]](validJson) shouldBe a[Right[_, _]]
