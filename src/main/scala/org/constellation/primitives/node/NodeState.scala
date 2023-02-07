c973df511dc31748796b6f08e43082ec84d49600
Tomasz Bekas <tomasz.bekas@gmail.com>
2023-02-07 14:36:31 +0100

    Update NodeState enum

--- src/main/scala/org/constellation/primitives/node/NodeState.scala
+++ src/main/scala/org/constellation/primitives/node/NodeState.scala
@@ -4,6 +4,7 @@ object NodeState extends Enumeration {
   type State = Value
 
   val Initial, ReadyToJoin, LoadingGenesis, GenesisReady, RollbackInProgress, RollbackDone, StartingSession,
-      SessionStarted, WaitingForDownload, DownloadInProgress, Observing, Ready, Leaving, Offline = Value
+      SessionStarted, WaitingForDownload, DownloadInProgress, WaitingForObserving, Observing, WaitingForReady, Ready,
+      Leaving, Offline = Value
 
 }
